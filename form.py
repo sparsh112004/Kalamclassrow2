34438287baab72cf684914b2e5f6d98ace671ba8
prakharavantika27 <165776683+prakharavantika27@users.noreply.github.com>
2024-04-06 16:14:54 +0530

    the course improved my knowledge.py
    the course substantially improved my knowledge

--- form.py
+++ form.py
@@ -35,4 +35,10 @@ pace=int(input("The pace at which material was covered was:\nEnter '5' if too fa
 material=int(input("The course materials helped me understand the subject matter:\nEnter '5' if you strongly agree\nEnter '4' if you agree\nEnter '3' if neutral\nEnter '2' if disagree\nEnter '1' if strongly disagree\n Your choice : "))
 skills=int(input("In this course, I learned skills or content that I consider valuable:\nEnter '5' if strongly agree\nEnter '4' if agree\nEnter '3' if neutral\nEnter '2' if disagree\nEnter '1' if strongly disagree\n Your choice : "))
 knowledge=int(input("What would you say about the knowledge of the course lead:\nEnter '5' if excellent\nEnter '4' if good\nEnter '3' if average\nEnter '2' if bad\nEnter '1' if very bad\n Your choice : "))
-overall=pace+material+skills+knowledge                  
+overall=pace+material+skills+knowledge  
+
+
+response = input("The course substantially improved my knowledge level. (agree/strongly agree/disagree/neutral): ")
+knowledge_improvement = response.lower()
+print("Impact on knowledge level:", knowledge_improvement)
+

148939d6370bf2ea449d286374a682892e61ad28
Jaysinghanjana1 <165776740+Jaysinghanjana1@users.noreply.github.com>
2024-04-06 10:32:06 +0000

    f

--- form.py
+++ form.py
@@ -1,1 +1,10 @@
-1111
+DT = input ("Enter the course :")
+POE = input ("Enter the course :")
+FOP = input ("Enter the course :")
+ITC = input ("Enter the course :")
+if(DT=="design thinking"):
+    elif(POE=="principal of engireeing"):
+        elif(FOP=="fandamentel of programing"):
+            elif(ITC=="introduction to computing"):
+                else:
+        
